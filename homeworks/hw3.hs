cb8ba005ce0ba0ec7c0b7f4f121b1a28fc34131c
Arthur Vardanyan <artie.vard@gmail.com>
2014-12-22 09:46:53 +0400

    add solution for 2.

--- homeworks/hw3.hs
+++ homeworks/hw3.hs
@@ -9,14 +9,14 @@ skips [1] == [[1]]
 skips [True,False] == [[True,False], [False]]
 skips [] == [] -}
 
--- 1.
+-- Exercise 1 Hopscotch
 skips :: [a] -> [[a]]
 skips xs =
-  skips' xs 0 l
+  skips' xs 0 len
   where
-    l = length xs
+    len = length xs
     skips' ys i n | i == n = []
-                  |otherwise = (everyNth i ys) : skips' ys (i+1) n
+                  |otherwise = everyNth i ys : skips' ys (i+1) n
 
 everyNth1 :: Int -> [a] -> [a]
 everyNth1 0 xs = xs
@@ -28,23 +28,14 @@ everyNth1 n xs =
    then []
    else  head ds : everyNth1 n (drop (n+1) xs)
 
-everyNth2 :: Int -> [a] -> [a]
-everyNth2 0 xs = xs
-everyNth2 n xs =
-  let
-    ds@(y:ys) = drop n xs
-  in
-   if null ds
-   then []
-   else  y : everyNth2 n (drop n ds)
 
 
 -- other approach for everyNth
 
 everyNth :: Int -> [a] -> [a]
 everyNth 0 _  = []
-everyNth 1 xs = xs
-everyNth n xs = everyNth' 1 n xs
+everyNth 1 ys = ys
+everyNth k ys = everyNth' 1 k ys
   where
     everyNth' :: Int -> Int -> [a] -> [a]
     everyNth' _ _ [] = []
@@ -52,3 +43,20 @@ everyNth n xs = everyNth' 1 n xs
       |cur == n = x : everyNth' 1 n xs
       |otherwise = everyNth' (cur+1) n xs
     
+
+
+-- Exercise 2 Local maxima
+
+localMaxima :: [Integer] -> [Integer]
+localMaxima [] = []
+localMaxima [_] = []
+localMaxima [_ , _] = []
+localMaxima xs@(_:x':xs') =
+  [ b | (a,b,c) <- zip3 xs (x':xs') xs', a < b && b > c]
+
+{--localMaxima' (a:b:c:cs)
+  |a < b && b > c = b : localMaxima' (b:c:cs)
+  |otherwse = localMaxima' (b:c:cs)
+--}
+
+-- Exercise 3 Histogram
