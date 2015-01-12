c2419f1d17debbad58736c00ef7c8221baa307e1
Arthur Vardanyan <artie.vard@gmail.com>
2015-01-12 21:23:33 +0400

    re-organise folder, add another instructor's course.

--- byorgey/homeworks/hw5tests.hs
+++ byorgey/homeworks/hw5tests.hs
@@ -16,4 +16,6 @@ main = hspec $ do
         `shouldBe` Just (Mul (Add (Lit 2) (Lit 3)) (Lit 4))
     it "evalStr function" $ do
       evalStr "(2+3)*4"  `shouldBe` Just 20
-   
+   -- it "Expr type class" $ do
+    -- (mul (add (lit 2) (lit 3)) (lit 4)) :: ExprT
+      --  `shouldBe` 20  -- ( Mul (Add (Lit 2) (Lit 3)) (Lit 4))
