b5cc5848e51878ea0a596c2ae440f567e9deefba
Arthur Vardanyan <artie.vard@gmail.com>
2015-01-22 15:24:42 +0400

    solve ex 1.

--- byorgey/homeworks/Hw07_JoinList.hs
+++ byorgey/homeworks/Hw07_JoinList.hs
@@ -1,13 +1,35 @@
 {-# OPTIONS_GHC -Wall #-}
-
 module Hw07_JoinList where
 
-data JoinListBasic a = Empty
-                     | Single a
-                     | Append (JoinListBasic a) (JoinListBasic a)
+import Data.Monoid
 
-jlbToList :: JoinListBasic a -> [a]
+data JoinList m a = Empty
+                  | Single m a
+                  | Append m (JoinList m a) (JoinList m a)
+  deriving (Eq, Show)
+           
+jlist :: JoinList (Product Int) Char
+jlist = Append (Product 210)
+        (Append (Product 30)
+         (Single (Product 5) 'y')
+         (Append (Product 6)
+          (Single (Product 2) 'e')
+          (Single (Product 3) 'a')))
+        (Single (Product 7) 'h')
+
+jlbToList :: JoinList m a -> [a]
 jlbToList Empty = []
-jlbToList (Single a) = [a]
-jlbToList (Append l1 l2) = jlbToList l1 ++ jlbToList l2                       
+jlbToList (Single _ a) = [a]
+jlbToList (Append _ l1 l2) = jlbToList l1 ++ jlbToList l2                       
+
+(+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
+(+++) Empty  jls = jls
+(+++) jls Empty = jls
+(+++) js1 js2  = Append (tag js1 `mappend` tag js2) js1 js2 
+
+tag :: Monoid m => JoinList m a -> m
+tag Empty = mempty
+tag (Single m _) = m
+tag (Append m _ _) = m
+
 
--- byorgey/homeworks/Hw07_Scrabble.hs
+++ byorgey/homeworks/Hw07_Scrabble.hs
@@ -1,3 +1,6 @@
 {-# OPTIONS_GHC -Wall #-}
 
 module Hw07_Scrabble where
+
+-- http://thepixiepit.co.uk/scrabble/rules.html
+-- Scrabble — «рыться в поисках чего-либо»
