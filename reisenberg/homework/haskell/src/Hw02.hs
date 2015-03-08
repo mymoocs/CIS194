0e335233d84099e85001dedd0498d8926884851d
Arthur Vardanyan <artie.vard@gmail.com>
2015-03-08 22:39:06 +0400

    solve ex1 add tests.

--- reisenberg/homework/haskell/src/Hw02.hs
+++ reisenberg/homework/haskell/src/Hw02.hs
@@ -4,11 +4,15 @@ Collaborators: <your collaborators here, or "none">
 Notes: <any particular notes about your work -- what you struggled with,
         what's not working, what's really cool, etc.>
 -}
+{-# OPTIONS_GHC -Wall #-}
 
-module HW02 where
+module Hw02 where
 
-import Words
-import Data.List
+import Hw02_Words
+import Data.List (elem, delete)
+
+import qualified Test.HUnit      as T
+import qualified Test.HUnit.Util as U
 
 -- Though a Scrabble hand is the same Haskell type as a Scrabble word, they
 -- have different properties. Specifically, a hand is unordered whereas a word
@@ -33,8 +37,56 @@ type Template = String
 type STemplate = Template
 
 -- Write your code below:
+
+--------------------------------------------------------------------------------
+--- Exercise 1.
+
+-- | 
+-- sts whether a certain word is formable from the tiles
+-- in a Scrabble hand.
+-- >>> formableBy "fun" ['x','n','i','f','u','e','l']
+-- True
+-- >>> formableBy "haskell" ['k','l','e','h','a','l','s']
+-- True
+-- >>> formableBy "haskell" ['k','l','e','h','a','y','s']
+-- False
+
 formableBy :: String -> Hand -> Bool
-formableBy = undefined
+formableBy [] _ = True
+formableBy (c:cs) hs |c `elem` hs = formableBy cs (delete c hs)
+                     |otherwise = False
+
+ex1 :: T.Test
+ex1 = T.TestList
+      [
+        U.teq "ex10" (formableBy "fun" ['x','n','i','f','u','e','l']) True
+      , U.teq "ex11" (formableBy "haskell" ['k','l','e','h','a','l','s']) True
+      , U.teq "ex12" (formableBy "haskell" ['k','l','e','h','a','y','s']) False
+      ]
+--------------------------------------------------------------------------------
+--- Exercise 2.
+
+-- |
+--
+-- >>> wordsFrom ['a','b','c','d']
+-- ["ab","ad","ba","bad","cab","cad","dab"]
+-- >>> wordsFrom ['h','e','l','l','o'] 
+-- ["eh","el","ell","he","hell","hello","helo","ho","hoe","hole","lo","oe","oh","ole"]
 
 wordsFrom :: Hand -> [String]
 wordsFrom hand = filter (`formableBy` hand) allWords
+
+ex2 :: T.Test
+ex2 = T.TestList
+      [
+        U.teq "ex20" (wordsFrom ['a','b','c','d']) (["ab","ad","ba","bad","cab","cad","dab"])
+      , U.teq "ex21" (wordsFrom ['h','e','l','l','o']) (["eh","el","ell","he","hell","hello","helo","ho","hoe","hole","lo","oe","oh","ole"])      
+      ]
+
+
+hw2 :: IO T.Counts
+hw2 = do
+  T.runTestTT ex1
+  T.runTestTT ex2
+  
+  
--- reisenberg/homework/haskell/src/Main.hs
+++ reisenberg/homework/haskell/src/Main.hs
@@ -2,6 +2,9 @@
 module Main where
 
 import Hw01 
-
+import Hw02
 main = do
+  print "homework 1"
   hw1
+  print "homework 2"
+  hw2
