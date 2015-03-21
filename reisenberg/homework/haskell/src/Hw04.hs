a7c8da60c8739da0305457bf27b0e09b70bd2d6e
Arthur Vardanyan <artie.vard@gmail.com>
2015-03-21 15:40:30 +0400

    done!.

--- reisenberg/homework/haskell/src/Hw04.hs
+++ reisenberg/homework/haskell/src/Hw04.hs
@@ -2,6 +2,11 @@
 
 module Hw04 where
 import Hw04_BST
+import qualified Test.HUnit      as T
+import qualified Test.HUnit.Util as U
+
+import Data.Char(isUpper) 
+
 --------------------------------------------------------------------------------
 --- Exercise 1.
 -- |
@@ -118,7 +123,149 @@ ex12 a = a
 -- |
 -- the insertion method for a binary search tree:
 -- >>> 
+{-
+data BST a = Leaf
+           | Node (BST a) a (BST a)
+  deriving Show
+-}
+
+compInt :: Int -> Int -> Ordering
+compInt x y |x > y = GT
+            |x < y = LT
+            |True  = EQ
+                     
+empty :: BST a
+empty = Leaf
 
 insertBST :: (a -> a -> Ordering) -> a -> BST a -> BST a
-insertBST = undefined
+insertBST _ a Leaf = Node Leaf a Leaf
+insertBST f k ts@(Node l a r) = case f k a of
+                              GT -> Node l a (insertBST f k r)
+                              LT -> Node (insertBST f k l) a r
+                              _  -> ts
 
+tree :: BST Int
+tree = Node (Node Leaf 1 Leaf) 2 (Node Leaf 5 Leaf)
+ex13 :: T.Test
+ex13 = T.TestList
+       [
+         U.teq "ex130" (insertBST compInt 2 empty) (Node Leaf 2 Leaf)
+       , U.teq "ex131" (insertBST compInt 8 tree ) (Node (Node Leaf 1 Leaf) 2 (Node Leaf 5 (Node Leaf 8 Leaf)))
+       ]
+
+
+--------------------------------------------------------------------------------
+--- Exercise 14.
+-- |
+-- Check to see if a list of strings contains only capitalized words
+-- >>> allCaps ["Hi","There"]
+-- True
+-- >>>allCaps []
+-- True
+-- >>>allCaps ["", "Blah"]
+-- False
+-- >>>allCaps ["Hi","there"]
+-- False
+
+
+-- safe helpeprs
+safeHead :: [a] -> Maybe a
+safeHead [] = Nothing
+safeHead (x:_) = Just x
+
+safeTail :: [a] -> Maybe [a]
+safeTail [] = Nothing
+safeTail (_:xs) = Just xs
+
+
+allCaps :: [String] -> Bool
+allCaps [] = True
+allCaps ys= all (\a->a == True) $ boolList ys
+  where
+    boolList [] = [True]
+    boolList (x:xs) = case safeHead x of
+                       Nothing -> [False]
+                       Just c  -> isUpper c : boolList xs
+
+ex14 :: T.Test
+ex14 = T.TestList
+       [
+         U.teq "ex140" (allCaps ["Hi","There"]) True
+       , U.teq "ex141" (allCaps []) True
+       , U.teq "ex142" (allCaps ["", "Blah"]) False
+       , U.teq "ex143" (allCaps ["Hi","there"]) False
+       ]
+
+--------------------------------------------------------------------------------
+--- Exercise 15.
+-- |
+-- Drop the trailing whitespace from a string:
+
+
+dropTrailingWhitespace :: String -> String
+dropTrailingWhitespace = filter (/= ' ')
+
+ex15 :: T.Test
+ex15 = T.TestList
+  [
+    U.teq "ex150" (dropTrailingWhitespace "foo") "foo"
+  , U.teq "ex151" (dropTrailingWhitespace "") ""
+  , U.teq "ex152" (dropTrailingWhitespace "bar ") "bar"
+  ]
+
+
+--------------------------------------------------------------------------------
+--- Exercise 16.
+
+-- |
+-- Get the first letter (if it exists) of a list of strings:
+-- >>>firstLetters ["foo", "bar"]
+--['f','b']
+-- >>>firstLetters ["alpha",""]
+--['a']
+-- >>>firstLetters []
+--[]
+-- >>>firstLetters ["",""]
+--[]
+firstLetters :: [String] -> [Char]
+firstLetters = map (\(x:_)->x)
+
+ex16 :: T.Test
+ex16 = T.TestList
+       [
+         U.teq "160" (firstLetters ["foo", "bar"]) ['f','b']
+         , U.teq "161" (firstLetters ["alpha",""]) ['a']
+         , U.teq "162" (firstLetters []) []
+         , U.teq "163" (firstLetters ["",""]) []
+       ]
+--------------------------------------------------------------------------------
+--- Exercise 17.
+-- |
+-- Render a proper bracketed list given a list of strings.
+-- >>>asList ["alpha","beta","gamma"]
+--"[alpha,beta,gamma]"
+-- >>>asList []
+--"[]"
+-- >>>asList ["lonely"]
+--"[lonely]"
+
+asList :: [String] -> String
+asList [] = "]"
+asList (x:xs) = ","++x++ asList xs -- TODO fix,
+
+ex17 :: T.Test
+ex17 = T.TestList
+       [
+         U.teq "ex170" (asList ["alpha","beta","gamma"]) "[alpha,beta,gamma]"
+       , U.teq "ex171" (asList []) "[]"
+       , U.teq "ex172" (asList ["lonely"]) "[lonely]"
+       ]
+
+hw4 :: IO T.Counts
+hw4 = do
+  T.runTestTT ex13
+  T.runTestTT ex14
+  T.runTestTT ex15
+  T.runTestTT ex16
+  T.runTestTT ex17
+  
--- reisenberg/homework/haskell/src/Main.hs
+++ reisenberg/homework/haskell/src/Main.hs
@@ -4,7 +4,9 @@ module Main where
 import Hw01 
 import Hw02
 import Hw03
-import Hw03Log -- for testing in ghci 
+import Hw03Log -- for testing in ghci
+import Hw04
+import Hw04_BST  -- for testing in ghci
 main = do
   print "homework 1"
   hw1
@@ -12,5 +14,6 @@ main = do
   hw2
   print "homework 3"
   hw3
-    
+  print "homework 4"
+  hw4
   
