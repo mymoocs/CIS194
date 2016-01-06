8b8bea7a404626050f31dc85ca478f8c11099379
Arthur Vardanyan <artie.vard@gmail.com>
2016-01-06 21:21:55 +0400

    ex 4 solved

--- byorgey/haskell/src/Cis194/Hw06_Fibonacci.hs
+++ byorgey/haskell/src/Cis194/Hw06_Fibonacci.hs
@@ -22,6 +22,7 @@ fibs1 = map fib [0..]
 -- requires only O(n) addition operations
 fibs2 :: [Integer]
 fibs2 = undefined
+        
 ------------------------------------------------------------------------------
 -- Exercise 3:
 
@@ -39,15 +40,62 @@ instance (Show a) => Show (Stream a) where
 streamToList :: Stream a -> [a]
 streamToList (x:.xs) = x:streamToList xs
 
-               
 ------------------------------------------------------------------------------
 -- Exercise 4:
 
-------------------------------------------------------------------------------
--- Exercise 5:
+-- | a function which generates a stream containing infinitely many copies of the
+--   given element.
+
+streamRepeat :: a -> Stream a
+streamRepeat e = e:.streamRepeat e
+
+-- | a function which applies a function to every element of a Stream.
+streamMap :: (a -> b) -> Stream a -> Stream b
+streamMap f (s:.ss) = f s :. streamMap f ss
+
+-- | a function which generates a Stream from a “seed” of type a, which is the first
+-- element of the stream, and an “unfolding rule” of type a -> a which specifies how
+-- to transform the seed into a new seed, to be used for generating the rest of the stream. 
+streamFromSeed :: (a -> a) -> a -> Stream a
+streamFromSeed f a = a:.streamFromSeed f (f a)
 
 ------------------------------------------------------------------------------
+-- Exercise 5:
+-- Define the streams
+
+-- | which contains the infinite list of natural numbers 0, 1, 2, . . .
+nats :: Stream Integer
+nats = streamFromSeed (+ 1) 0
+
+-- | which corresponds to the ruler function 0,1,0,2,0,1,0,3,0,1,0,2,0,1,0,4,...
+ruler :: Stream Integer
+ruler = interleaveStreams zeros maxPowers2
+        where
+          maxPowers2 = streamMap maxDivisor evens
+                       
+evens :: Stream Integer
+evens = streamFromSeed (+ 2) 2
+        
+zeros :: Stream Integer         
+zeros = streamRepeat 0
+        
+interleaveStreams :: Stream a -> Stream a -> Stream a
+interleaveStreams (a:.as) (b:.bs) = a:.b:.interleaveStreams as bs 
+
+                                    
+power2 = [ 2^x | x <- [1..] ]
+
+maxDivisor :: Integer -> Integer         
+maxDivisor = maxDivisor' 0
+    where
+      maxDivisor' :: Int  -> Integer -> Integer
+      maxDivisor' i n
+           | mod n (power2 !! i) /= 0 = fromIntegral i
+           | otherwise = maxDivisor' (i+1) n
+                         
+------------------------------------------------------------------------------
 -- Exercise 6:
+
 
 ------------------------------------------------------------------------------
 -- Exercise 7:
