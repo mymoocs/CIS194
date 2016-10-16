9196ddedf35bc11de8147332e2406c8579e2be5b
Arthur Vardanyan <artie.vard@gmail.com>
2016-10-16 15:25:35 +0200

    ex 1 and 2 done

--- byorgey/racket/2016/Hw01.rkt
+++ byorgey/racket/2016/Hw01.rkt
@@ -1,7 +1,39 @@
 #lang racket
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Exercise 1
 
+; convert positive Integers to a list of digits
 (define toDigits
   (lambda (n)
-  (+ n 2))
- )
+    (if (<= n 0)
+        '()
+        (append (digitsExceptLast n) (list (remainder n 10)))
+    )))
+
+(define  digitsExceptLast
+  (lambda (n)
+    (toDigits (quotient n 10))
+    ))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Exercise 2
+
+; doouble every other sarting from right
+(define (doubleEveryOther lst)
+  (define (doubleEven xs)
+      (cond
+        [(empty? xs) '()]
+        [else (cons (first xs) (doubleOdd (rest xs)))])
+      )
+
+  (define (doubleOdd xs)
+    (cond
+      [(empty? xs) '()]
+      [(cons (* 2 (first xs)) (doubleEven (rest xs)))])
+    )
+
+  (cond
+    [(even? (length lst)) (doubleOdd lst)]
+    [else (doubleEven lst)])
+  )
