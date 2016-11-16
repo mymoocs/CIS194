f260c909da72d4eeb5d98fa6aa29fae0028bfada
Arthur Vardanyan <artie.vard@gmail.com>
2016-11-16 14:43:44 +0100

    add testy

--- byorgey/haskell/2016/src/Cis194.hs
+++ byorgey/haskell/2016/src/Cis194.hs
@@ -2,4 +2,4 @@ module Cis194 (module X) where
 
 import Cis194.Hw01             as X
 import Cis194.Hw02             as X
-import Cis194.Hw03            as X
+import Cis194.Hw03.Golf            as X
